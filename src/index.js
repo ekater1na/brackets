45b70ac7e95ca5a749857ab3f239934c6e516cbb
ekater1na <ekaterina_l@tut.by>
2019-03-24 01:27:14 +0300

    feat: add function to check sequence of brackets (17/20 tests passed)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+     
+    let stack = [];
+
+    let left = {
+        '{': '}',
+        '[': ']',
+        '(': ')'
+    };
+
+    let right = {
+        '}': true,
+        ']': true,
+        ')': true
+    }
+
+    for (let i = 0; i < str.length; i++) {
+
+        let char = str[i];
+
+        if (left[char]) {
+            stack.push(char);
+        } else if (right[char]) {
+            if (left[stack.pop()] !== char) return false;
+        }
+    }
+    return stack.length === 0;
 }
